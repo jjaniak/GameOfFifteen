cdac52701cfe7cccaa80ee7f96a361462523b595
jjaniak <julia.janiak@gmail.com>
2020-01-15 19:27:25 +0100

    Tests: create drafts of tests checking game board behaviour

---
+++ src/test/java/com/griddynamics/GameBoardTests.java
@@ -0,0 +1,56 @@
+package com.griddynamics;
+
+import org.junit.jupiter.api.Test;
+
+public class GameBoardTests {
+
+    @Test
+    public void checkShufflingConfigurationWorks() {
+
+    }
+
+    @Test
+    public void checkIsGameSolvedMethod() {
+
+    }
+
+    @Test
+    public void moveRightCorrectly() {
+
+    }
+
+    @Test
+    public void moveRightIncorrectly() {
+
+    }
+
+    @Test
+    public void moveLeftCorrectly() {
+
+    }
+
+    @Test
+    public void moveLeftIncorrectly() {
+
+    }
+
+    @Test
+    public void moveUptCorrectly() {
+
+    }
+
+    @Test
+    public void moveUpIncorrectly() {
+
+    }
+
+    @Test
+    public void moveDownCorrectly() {
+
+    }
+
+    @Test
+    public void moveDownIncorrectly() {
+
+    }
+}
