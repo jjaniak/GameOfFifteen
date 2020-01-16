c1a8db7624b0506c57da37b411779168fbe77a18
jjaniak <julia.janiak@gmail.com>
2020-01-16 17:41:15 +0100

    Tests: add tests checking board functioning (all types of movements, shuffling and checking if game is solved)

--- src/test/java/com/griddynamics/GameBoardTests.java
+++ src/test/java/com/griddynamics/GameBoardTests.java
@@ -1,56 +1,98 @@
 package com.griddynamics;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class GameBoardTests {
 
+    private final Board SolvedBoard = new Board(Board.SOLVED_CONFIGURATION);
+
+    private final int[][] mixedConfiguration = new int[][]{
+            {11, 15, 10,  8},
+            { 2,  0,  7,  5},
+            { 9,  4,  1, 12},
+            { 3, 14, 13,  6} };
+    private Board board = new Board(mixedConfiguration);
+
+
     @Test
     public void checkShufflingConfigurationWorks() {
+        assertTrue(board.isBoardIdentical(mixedConfiguration));
 
+        board.shuffleBoardConfiguration();
+        assertFalse(board.isBoardIdentical(mixedConfiguration));
     }
 
     @Test
     public void checkIsGameSolvedMethod() {
+        SolvedBoard.moveUp();
+        assertFalse(SolvedBoard.isGameSolved());
+
+        SolvedBoard.moveDown();
+        assertTrue(SolvedBoard.isGameSolved());
 
     }
 
     @Test
     public void moveRightCorrectly() {
-
+        assertTrue(board.moveRight());
+        assertEquals(board.getConfiguration()[1][2], 0);
+        assertEquals(board.getConfiguration()[1][1], 7);
     }
 
     @Test
     public void moveRightIncorrectly() {
+        board.moveRight();
+        board.moveRight();
 
+        assertFalse(board.moveRight());
+        assertEquals(board.getConfiguration()[1][3], 0);
     }
 
     @Test
     public void moveLeftCorrectly() {
-
+        assertTrue(board.moveLeft());
+        assertEquals(board.getConfiguration()[1][0], 0);
+        assertEquals(board.getConfiguration()[1][1], 2);
     }
 
     @Test
     public void moveLeftIncorrectly() {
+        board.moveLeft();
 
+        assertFalse(board.moveLeft());
+        assertEquals(board.getConfiguration()[1][0], 0);
     }
 
     @Test
     public void moveUptCorrectly() {
-
+        assertTrue(board.moveUp());
+        assertEquals(board.getConfiguration()[0][1], 0);
+        assertEquals(board.getConfiguration()[1][1], 15);
     }
 
     @Test
     public void moveUpIncorrectly() {
+        board.moveUp();
 
+        assertFalse(board.moveUp());
+        assertEquals(board.getConfiguration()[0][1], 0);
     }
 
     @Test
     public void moveDownCorrectly() {
-
+        assertTrue(board.moveDown());
+        assertEquals(board.getConfiguration()[2][1], 0);
+        assertEquals(board.getConfiguration()[1][1], 4);
     }
 
     @Test
     public void moveDownIncorrectly() {
+        board.moveDown();
+        board.moveDown();
+
+        assertFalse(board.moveDown());
+        assertEquals(board.getConfiguration()[3][1], 0);
 
     }
 }
