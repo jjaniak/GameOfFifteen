5876f2c49e3ac926d3f7d4962e5a98e99d8e533a
jjaniak <julia.janiak@gmail.com>
2020-01-17 01:29:58 +0100

    Board: make SOLVED_CONFIGURATION more readable

--- src/main/java/com/griddynamics/Board.java
+++ src/main/java/com/griddynamics/Board.java
@@ -14,7 +14,12 @@ public class Board {
         }
     }
 
-    static final int[][] SOLVED_CONFIGURATION = { {1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0} };
+    static final int[][] SOLVED_CONFIGURATION = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 0}};
+
     private int[][] configuration;
     private Position emptyTile;
 
