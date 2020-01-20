ec45479a1455df65ea7fad64329a8eafd60c856e
jjaniak <julia.janiak@gmail.com>
2020-01-20 19:44:12 +0100

    OutputProcessor: make class package private and remove old constant

--- src/main/java/com/griddynamics/OutputProcessor.java
+++ src/main/java/com/griddynamics/OutputProcessor.java
@@ -7,8 +7,7 @@ import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.ArrayList;
 
-public class OutputProcessor {
-    private static final String filePath = "";
+class OutputProcessor {
 
     static final String FILE_PATH = "src/test/resources/file.txt";
     static final String NEW_LINE = System.getProperty("line.separator");
