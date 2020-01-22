52cd995079b39f9245017473d6fe5cb45fc490e0
jjaniak <julia.janiak@gmail.com>
2020-01-22 13:39:37 +0100

    OutputTests: remove some draft tests and refactor

--- src/test/java/com/griddynamics/OutputTests.java
+++ src/test/java/com/griddynamics/OutputTests.java
@@ -9,18 +9,14 @@ import java.nio.file.Paths;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.io.FileMatchers.*;
-import static org.hamcrest.Matchers.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 
 public class OutputTests {
 
     private static final String TEST_FILE_PATH = "src/test/resources/TestFile.txt";
 
-//   test just one class or run main inside the tests? Run main in order to generate output file."
-
     @AfterEach
-    public void deleteFile() throws IOException {
+    public void deleteFile() {
         try {
             System.out.println("deleting file");
             Files.deleteIfExists(Paths.get(TEST_FILE_PATH));
@@ -33,51 +29,13 @@ public class OutputTests {
 
     @Test
     public void fileShouldExistAndNotEmpty() {
-        OutputProcessor printer = new OutputProcessor();
-        int[][] conf = new int[][]{
-                { 1,  2,  3,  4},
-                { 5,  7,  0,  8},
-                { 9,  6, 10, 11},
-                {13, 14, 15, 12}};
-
-        Board easyBoard = new Board(conf);
-        printer.addToFile(TEST_FILE_PATH, easyBoard.toString());
+        OutputProcessor printer = new FileOutputProcessor(TEST_FILE_PATH);
+        printer.appendLine("Just some input to check if file is not empty");
 
         File file = new File(TEST_FILE_PATH);
 
         assertThat(file, anExistingFile());
         assertThat(file, aReadableFile());
-//        find an assertion to check if file is not empty
-    }
-
-    @Test
-    public void outputShouldContainInitialConfiguration() {
-        OutputProcessor printer = new OutputProcessor();
-        int[][] conf = new int[][]{
-                { 1,  2,  3,  4},
-                { 5,  7,  0,  8},
-                { 9,  6, 10, 11},
-                {13, 14, 15, 12}};
-
-        Board easyBoard = new Board(conf);
-        printer.addToFile(TEST_FILE_PATH, "Initial configuration: \n\n" + easyBoard);
-
-        File file = new File(TEST_FILE_PATH);
-
-    }
-
-    @Test
-    public void outputShouldContainCorrectFinalConfiguration() {
-    }
-
-    @Test
-    public void checkIfMovesNumberPresentAndValid() {
-    }
-
-
-    // ?Should be included in GameSolving, Output or GameLauncher tests?
-    @Test
-    public void shouldProperlyPrintNotSolvable() {
-//     print -1 to file for not solvable
+        // find an assertion to check if file is not empty
     }
 }
